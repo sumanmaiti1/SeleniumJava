f2763e2f6c4c1bb46126f1f6fcd45fdaa7c7a7af
Suman Maiti <sumanmaiti1@gmail.com>
2024-10-03 00:32:33 +0530

    SM:Java Selenium Tutorial

---
+++ tutorial/src/main/java/tutorial/FirstSeleniumScript.java
@@ -0,0 +1,45 @@
+package tutorial;
+
+import java.io.File;
+import java.util.Collections;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeDriverService;
+import org.openqa.selenium.edge.EdgeOptions;
+
+public class FirstSeleniumScript {
+
+    public static void main(String[] args) {
+
+        //--------- Creating a file object of Edge Driver 
+        File driverFile = new File("C:\\Users\\Radhe_Radhe\\Downloads\\msedgedriver.exe");
+        //--------- Creating a EdgeDriverService object for Edge Driver 
+        EdgeDriverService service = new EdgeDriverService.Builder().usingDriverExecutable(driverFile).build();
+
+        //--------- Creating Edge Option class 
+        EdgeOptions option = new EdgeOptions();
+
+        //--------This is to prevent "Microsoft Edge is being controlled by automated test software"
+        option.setExperimentalOption("useAutomationExtension", false);
+        option.setExperimentalOption("excludeSwitches",Collections.singletonList("enable-automation"));
+
+        //----------- Creating webdriver using service----------
+        WebDriver driver = new EdgeDriver(service,option);
+
+        //----------Maximize the browser
+        driver.manage().window().maximize();
+
+        //--------opening google.com
+        driver.get("https://google.com");
+
+        //---------verifying if google.com is populated successfully by checking the Page Title----------
+        if (driver.getTitle().equals("Google")) {
+            System.out.println("Google page is successfully loaded");
+        } else{
+            System.out.println("Google page isn't successfully loaded");
+        }
+        //----------- Quiting Webdriver Edge instance
+        driver.quit();
+    }
+}
